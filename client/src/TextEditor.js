160d4ca61b63c4394e52c6501d2145f424a00098
sherman0611 <sherman0611@gmail.com>
2024-10-18 15:56:38 +0100

    first commit

---
+++ client/src/TextEditor.js
@@ -0,0 +1,111 @@
+import React, { useCallback, useEffect, useState } from 'react'
+import Quill from 'quill'
+import "quill/dist/quill.snow.css"
+import { io } from 'socket.io-client'
+import { useParams } from 'react-router-dom'
+
+const SAVE_INTERVAL = 2000
+const TOOLBAR_OPTIONS = [
+    [{ header: [1, 2, 3, 4, 5, 6, false] }],
+    [{ font: [] }],
+    ["bold", "italic", "underline"],
+    [{ list: "ordered" }, { list: "bullet" }],
+    [{ color: [] }, { background: [] }],
+    [{ script: "sub" }, { script: "super" }],
+    [{ align: "" }, { align: "center" }, { align: "right" },  { align: "justify" }],
+    ["image", "blockquote", "code-block"],
+    ["clean"],
+]
+
+function TextEditor() {
+    const {id: documentId} = useParams()
+    const [socket, setSocket] = useState()
+    const [quill, setQuill] = useState()
+
+    // connect to server
+    useEffect(() => {
+        const s = io("http://localhost:3001")
+        setSocket(s)
+
+        return () => {
+            s.disconnect()
+        }
+    }, [])
+
+    useEffect(() => {
+        if (socket == null || quill == null) return
+        
+        socket.once("load-document", document => {
+            quill.setContents(document)
+            quill.enable()
+        })
+
+        socket.emit("get-document", documentId)
+
+    }, [socket, quill, documentId])
+
+    // auto save
+    useEffect(() => {
+        if (socket == null || quill == null) return
+
+        const interval = setInterval(() => {
+            socket.emit("save-document", quill.getContents())
+        }, SAVE_INTERVAL)
+
+        return () => {
+            clearInterval(interval)
+        }
+    }, [socket, quill])
+
+    // handle received text change event
+    useEffect(() => {
+        if (socket == null || quill == null) return
+
+        const handler = (delta) => {
+            quill.updateContents(delta)
+        }
+
+        socket.on("receive-changes", handler)
+
+        return () => {
+            socket.off("receive-changes", handler)
+        }
+    }, [socket, quill])
+
+    // send text change event
+    useEffect(() => {
+        if (socket == null || quill == null) return
+
+        const handler = (delta, oldDelta, source) => {
+            if (source !== 'user') return
+            socket.emit("send-changes", delta)
+        }
+
+        quill.on("text-change", handler)
+
+        return () => {
+            quill.off("text-change", handler)
+        }
+    }, [socket, quill])
+
+    // set up quill editor
+    const wrapperRef = useCallback(wrapper => {
+        if (wrapper == null) return
+
+        wrapper.innerHTML = ""
+        const editor = document.createElement('div')
+        wrapper.append(editor)
+        const q = new Quill(editor, {
+            theme: "snow", 
+            modules: { toolbar: TOOLBAR_OPTIONS }
+        })
+        q.enable(false)
+        setQuill(q)
+    }, [])
+    
+    return (
+        <div className="container" ref={wrapperRef}></div>
+    )
+}
+
+export default TextEditor
